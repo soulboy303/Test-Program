8de773965991457301d9309f0a02e6c0915a2127
soulboy303 <darrigo@asu.edu>
2026-03-17 05:26:57 -0700

    Changed for loop to while loop and set up outputs.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>;
-
+#include <string>;
+#include <vector>;
+#include <algorithm>;
 using namespace std;
 
 int main(){
 
+string input;
 
+cout << "Please insert string" << endl;
+cin >> input;
+
+vector<char> testVector(input.begin(), input.end());
+
+int left = 0;
+int right = testVector.size()-1;
+bool palindrome = true;
+
+while(left <= right){
+
+
+    if(testVector[left] != testVector[right]){
+        palindrome = false;
+        break;
+    }
+}
+
+if(palindrome){
+    cout << "This string is a palindrome" << endl;
+}
+else{
+    cout << "This string is not a palindrome" << endl;
+}
 
 }
