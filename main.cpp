4deb04c5227b4696bd6c46b42ba3c3c43047022a
soulboy303 <darrigo@asu.edu>
2026-03-17 05:13:39 -0700

    Decided code will be Paladrome checker. Added input cin and vector.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>;
+#include <string>;
+#include <vector>;
+#include <algorithm>;
 
 using namespace std;
 
 int main(){
 
+string input;
 
+cout << "Please insert string" << endl;
+cin >> input;
+
+vector<char> testVector(input.begin(), input.end());
+
+for(int i = 0; i < testVector.size(); i++){
+
+    
+
+}
 
 }
